0f8c66c96c0bba8ab74d19ac649fec3ca05f5c49
Mahmoud Ahmed <developermahmoud91@gmail.com>
2024-02-04 14:37:25 +0200

    add animation for welcome img

--- src/ShipperApp.jsx
+++ src/ShipperApp.jsx
@@ -1,7 +1,7 @@
 import Header from "./component/header/Header";
 function App() {
   return (
-    <div className="shipper-app">
+    <div className="shipper-app" style={{overflow: 'hidden'}}>
       <Header />
     </div>
   );
